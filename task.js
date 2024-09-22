b94ddb13ec94f1121f34c341017004b5e023ce22
HasanAli44 <mdhasanali8439@gmail.com>
2024-09-22 10:38:52 +0600

    4number multiply

---
+++ task.js
@@ -0,0 +1,9 @@
+function multiply(num1, num2, num3, num4) {
+  const result = num1 * num2 * num3 * num4;
+  return result;
+}
+const result = multiply(10, 2, 1, 5);
+
+// console.log(result);
+
+// task2
--- task1.js
+++
@@ -1,8 +0,0 @@
-function square(number) {
-  const borgo = number * 4;
-  console.log(borgo);
-}
-square(10);
-square(20);
-square(30);
-square(40);
