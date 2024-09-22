040761a10e4a485904f927489fd971e319eae952
HasanAli44 <mdhasanali8439@gmail.com>
2024-09-22 10:50:40 +0600

    oddEven check function

--- task.js
+++ task.js
@@ -7,3 +7,14 @@ const result = multiply(10, 2, 1, 5);
 // console.log(result);
 
 // task2
+function oddEven(number) {
+  if (number % 2 === 1) {
+    const result = number * 2;
+    return result;
+  } else {
+    const result = number / 2;
+    return result;
+  }
+}
+
+console.log(oddEven(21));
